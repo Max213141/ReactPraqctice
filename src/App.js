a5637f23b9147e1e10034ad9ed87775622816616
Max213141 <66839835+Max213141@users.noreply.github.com>
2020-10-27 21:32:48 +0300

    Make componts to disapear by click and achange header change by input

--- src/App.js
+++ src/App.js
@@ -20,8 +20,23 @@ class App extends Component {
     })
   }
 
-  changeTitleHandler = pageTitle =>{
-    this.setState({pageTitle})
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({
+      cars: cars
+    })
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index,1)
+
+    this.setState({
+      cars
+    })
   }
 
   render() {
@@ -38,7 +53,8 @@ class App extends Component {
                 key = {index}
                 name = {car.name}
                 year = {car.year}
-                onChangeTitle = { () => this.changeTitleHandler(car.name)}
+                onDelete = {this.deleteHandler.bind(this, index)}
+                onChangeName = { event => this.onChangeName(event.target.value, index)}
             />
         )
       })
